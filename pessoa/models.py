717efee947a0b6f35ba17dfb7f7c027e2c2a66cc
Isaque Kaio <isaque.rodrigues@navi.ifrn.edu.br>
2020-12-18 21:36:02 -0300

    Definindo os usuarios em app pessoa.

--- pessoa/models.py
+++ pessoa/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class User(AbstractUser):
+    is_coordenador = models.BooleanField(default=False)
+    is_profissional = models.BooleanField(default=False)
+    is_paciente = models.BooleanField(default=False)
