448f4f9c804caea130f6188617fc7bdfacb22212
Isaque Kaio <isaque.rodrigues@navi.ifrn.edu.br>
2020-12-19 19:23:04 -0300

    Pivotando o projeto, para corrigir detalhes.

--- modulo_administrativo/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ModuloAdministrativoConfig(AppConfig):
-    name = 'modulo_administrativo'
--- moduloagendamento/apps.py
+++ moduloagendamento/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class ModuloAgendamentoConfig(AppConfig):
-    name = 'modulo_agendamento'
+    name = 'moduloagendamento'
