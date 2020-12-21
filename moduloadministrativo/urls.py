2146d87b6bb86025c794e79daad6a04cbc31b4ee
Isaque Kaio <isaque.rodrigues@navi.ifrn.edu.br>
2020-12-21 08:14:17 -0300

    Removendo código repetido.

--- moduloadministrativo/urls.py
+++ moduloadministrativo/urls.py
@@ -17,10 +17,6 @@ urlpatterns = [
     path('fabricante/atualizar/<int:pk>', atualizar_fabricante, name='atualizar_fabricante'),
     path('fabricante/excluir/<int:pk>', excluir_fabricante, name='excluir_fabricante'),
 
-    path('vacina', listar_vacina, name='listar_vacina'),
-    path('vacina/cadastrar', cadastrar_vacina, name='cadastrar_vacina'),
-    path('vacina/atualizar/<int:pk>', atualizar_vacina, name='atualizar_vacina'),
-
     path('profissional', listar_profissional, name='listar_profissional'),
     path('profissional/cadastrar', cadastrar_profissional, name='cadastrar_profissional'),
     path('profissional/atualizar/<int:pk>', atualizar_profissional, name='atualizar_profissional'),
